d45fda5ab76858cd12ba68fd0d6befcf55ae73a5
Allen Anthes <abanthes@gmail.com>
2019-05-20 12:18:52 -0500

    Add models for managed tables to migrate old data to

--- src/api/models.py
+++ src/api/models.py
@@ -51,7 +51,7 @@ class ArInternalMetadata(models.Model):
         db_table = "ar_internal_metadata"
 
 
-class CodeSchool(models.Model):
+class OldCodeSchool(models.Model):
     name = models.CharField(max_length=256, blank=True, null=True)
     url = models.CharField(max_length=256, blank=True, null=True)
     logo = models.CharField(max_length=256, blank=True, null=True)
@@ -73,6 +73,29 @@ class CodeSchool(models.Model):
     class Meta:
         managed = False
         db_table = "code_schools"
+
+
+class CodeSchool(models.Model):
+    name = models.CharField(max_length=256, blank=True, null=True)
+    url = models.CharField(max_length=256, blank=True, null=True)
+    logo = models.CharField(max_length=256, blank=True, null=True)
+    full_time = models.BooleanField(blank=True, null=True)
+    hardware_included = models.BooleanField(blank=True, null=True)
+    has_online = models.BooleanField(blank=True, null=True)
+    online_only = models.BooleanField(blank=True, null=True)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    notes = models.TextField(blank=True, null=True)
+    mooc = models.BooleanField()
+    is_partner = models.BooleanField()
+    rep_name = models.CharField(max_length=256, blank=True, null=True)
+    rep_email = models.CharField(max_length=256, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.url}"
+
+    class Meta:
+        db_table = "api_code_schools"
 
 
 class Event(models.Model):
@@ -136,6 +159,33 @@ class GitHubUser(models.Model):
         db_table = "git_hub_users"
 
 
+class OldLocation(models.Model):
+    va_accepted = models.BooleanField(blank=True, null=True)
+    address1 = models.CharField(max_length=256, blank=True, null=True)
+    address2 = models.CharField(max_length=256, blank=True, null=True)
+    city = models.CharField(max_length=256, blank=True, null=True)
+    state = models.CharField(max_length=256, blank=True, null=True)
+    zip = models.IntegerField(blank=True, null=True)
+    code_school = models.ForeignKey(
+        OldCodeSchool,
+        models.DO_NOTHING,
+        blank=True,
+        null=True,
+        related_name="locations",
+    )
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+    def __str__(self):
+        return (
+            f"{self.code_school} - {self.address1} {self.city} {self.state} {self.zip}"
+        )
+
+    class Meta:
+        managed = False
+        db_table = "locations"
+
+
 class Location(models.Model):
     va_accepted = models.BooleanField(blank=True, null=True)
     address1 = models.CharField(max_length=256, blank=True, null=True)
@@ -155,8 +205,7 @@ class Location(models.Model):
         )
 
     class Meta:
-        managed = False
-        db_table = "locations"
+        db_table = "api_locations"
 
 
 class Request(models.Model):
@@ -306,7 +355,7 @@ class Tag(models.Model):
         db_table = "tags"
 
 
-class TeamMember(models.Model):
+class OldTeamMember(models.Model):
     name = models.CharField(max_length=256, blank=True, null=True)
     role = models.CharField(max_length=256, blank=True, null=True)
     created_at = models.DateTimeField()
@@ -322,6 +371,23 @@ class TeamMember(models.Model):
     class Meta:
         managed = False
         db_table = "team_members"
+
+
+class TeamMember(models.Model):
+    name = models.CharField(max_length=256, blank=True, null=True)
+    role = models.CharField(max_length=256, blank=True, null=True)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    description = models.TextField(blank=True, null=True)
+    group = models.CharField(max_length=256, blank=True, null=True)
+    image_src = models.CharField(max_length=256, blank=True, null=True)
+    email = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.role}"
+
+    class Meta:
+        db_table = "api_team_members"
 
 
 class Vote(models.Model):
