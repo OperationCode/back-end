88e85b91fbbb45a110d7ec25f1ae7fcda569cb71
wimo7083 <wimo7083@colorado.edu>
2019-05-27 14:52:15 -0600

    include the testing config for prometheus with the db hookup

--- src/settings/testing.py
+++ src/settings/testing.py
@@ -11,7 +11,7 @@ INSTALLED_APPS = INSTALLED_APPS + ["tests"]  # noqa: F405
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
+        "ENGINE": "django_prometheus.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),  # noqa: F405
     }
 }
