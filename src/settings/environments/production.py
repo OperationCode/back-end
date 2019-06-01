5691143c89b643f45212c600028ae03d52e2738e
Allen Anthes <abanthes@gmail.com>
2019-05-31 21:32:42 -0500

    Change codeschool issue Github Repo

--- src/settings/environments/production.py
+++ src/settings/environments/production.py
@@ -41,4 +41,4 @@ STATICFILES_STORAGE = "custom_storages.StaticStorage"
 DEFAULT_FILE_STORAGE = "custom_storages.MediaStorage"
 
 # Temporary frontend configs
-GITHUB_REPO = "OperationCode/back-end"
+GITHUB_REPO = "OperationCode/operationcode_backend"
