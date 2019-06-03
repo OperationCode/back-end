5ebe0042c37998fc42b3af74e1160edec82ea444
Allen Anthes <abanthes@gmail.com>
2019-06-03 12:11:21 -0500

    Add pytest-env

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,12 +1,6 @@
-import os
-
 import django  # noqa: F402
 
 from .fixtures import *  # noqa: F402, F403
-
-os.environ["ENVIRONMENT"] = "TEST"
-os.environ.setdefault("DJANGO_ENV", "testing")
-os.environ["DJANGO_SETTINGS_MODULE"] = "settings"
 
 
 def pytest_configure(config):
