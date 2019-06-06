2ff7475056ba2625e5bb34570500e81c351af025
Allen Anthes <abanthes@gmail.com>
2019-06-06 13:54:14 -0500

    Update template deps

--- src/core/admin.py
+++ src/core/admin.py
@@ -11,7 +11,8 @@ admin.site.unregister(User)
 class ExtendedUserAdmin(UserAdmin):
     list_display = ("email", "first_name", "last_name", "is_staff", "is_superuser")
 
-    def suit_row_attributes(self, obj, request):
+    @staticmethod
+    def suit_row_attributes(obj, request):
         """
         Adds highlight to superusers in Users table
         """
