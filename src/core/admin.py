cb000eaf615c7b7dd44786dea4753b45ed162ea0
Allen Anthes <abanthes@gmail.com>
2019-06-10 10:00:02 -0500

    Ignore admin panel customizations in coverage

--- src/core/admin.py
+++ src/core/admin.py
@@ -12,7 +12,7 @@ class ExtendedUserAdmin(UserAdmin):
     list_display = ("email", "first_name", "last_name", "is_staff", "is_superuser")
 
     @staticmethod
-    def suit_row_attributes(obj, request):
+    def suit_row_attributes(obj, request):  # pragma: no cover
         """
         Adds highlight to superusers in Users table
         """
