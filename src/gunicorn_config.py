b4e3e05c59b039d24e72229d023c5bafb04be91a
Irving Popovetsky <irving@popovetsky.com>
2020-03-03 13:17:12 -0800

    Post-cutover cleanups to the back-end (#286)
    * add the django-health-check module and a healthz endpoint
    
    Signed-off-by: Irving Popovetsky <irving@popovetsky.com>
    
    * Add a custom_sampler to ignore all traces to the health check
    
    Signed-off-by: Irving Popovetsky <irving@popovetsky.com>
    
    * make sure to add honeycomb auto-instrumentation for dev environments or else youll have a bad day trying to troubleshoot
    
    Signed-off-by: Irving Popovetsky <irving@popovetsky.com>
    
    * Correct the ALLOWED_HOST values for prod and staging
    
    Signed-off-by: Irving Popovetsky <irving@popovetsky.com>
    
    * linter did its thing
    
    Signed-off-by: Irving Popovetsky <irving@popovetsky.com>

--- src/gunicorn_config.py
+++ src/gunicorn_config.py
@@ -218,6 +218,21 @@ def worker_abort(worker):
     worker.log.info("worker received SIGABRT signal")
 
 
+def sampler(fields):
+    request_path = fields.get("request.path")
+    response_code = fields.get("response.status_code")
+
+    # never sample errors
+    if response_code and response_code >= 500:
+        return True, 1
+    else:
+        # never capture healthy health checks
+        if request_path == "/healthz":
+            return False, 0
+        # catchall
+        return True, 1
+
+
 # Added for Honeycomb instrumentation
 def post_worker_init(worker):
     worker.log.info("beeline initialization in process pid %s", worker.pid)
@@ -229,5 +244,7 @@ def post_worker_init(worker):
         beeline.init(
             writekey=os.getenv("HONEYCOMB_WRITEKEY"),
             dataset=os.getenv("HONEYCOMB_DATASET"),
+            service_name="backend",
+            sampler_hook=sampler,
             debug=False,
         )
